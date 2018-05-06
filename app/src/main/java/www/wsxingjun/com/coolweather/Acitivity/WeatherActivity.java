daab73a9a6a3245b4375559d5ae162f13a5add53
wsxingjun <1185889308@qq.com>
2018-05-06 10:49:46 +0800

    v5 -增加了APP与状态栏的融合 【bug】listView仍然不可以显示；天气显示的界面中的预报选项存在问题

--- app/src/main/java/www/wsxingjun/com/coolweather/Acitivity/WeatherActivity.java
+++ app/src/main/java/www/wsxingjun/com/coolweather/Acitivity/WeatherActivity.java
@@ -1,7 +1,9 @@
 package www.wsxingjun.com.coolweather.Acitivity;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.media.Image;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -45,7 +47,16 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //设置状态栏和APP融合效果
+        if (Build.VERSION.SDK_INT >= 21){
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.layout_weather);
+
 
 //        initView();
         imge_everyDayImage = (ImageView) findViewById(R.id.imge_everyDayImage);
